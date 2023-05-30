53a516474cb59d8a9488b11007964d792cb1cc49
DamoFD <voshall@yahoo.com>
2023-05-29 21:44:18 -0700

    post page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,19 @@
 
 @section('content')
 
-
-    <h2>{{ $post['title'] }}</h2>
-    <p>{{ $post['description'] }}</p>
+<section id="post">
+    <a href="/reno-artistry/public">Back</a>
+    <img src="{{ asset('images/reno-artistry-logo.webp') }}" />
+    <h1>{{ $post->title }}</h1>
+    <h2>John Doe</h2>
+    <ul>
+        <li>watercolor</li>
+        <li>painting</li>
+        <li>landscape</li>
+    </ul>
+    <p>{{ $post->description }}</p>
+    <a href="mailto:{{ $post->email }}">Contact</a>
+    <a href="{{ $post->website }}">View Website</a>
+</section>
 
     @endsection
