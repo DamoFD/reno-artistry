0bb83dc21452b17f970273cccb33dbe2f7a2ccf7
DamoFD <voshall@yahoo.com>
2023-05-31 00:29:41 -0700

    user post relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -21,5 +22,10 @@ class Post extends Model
             ->orWhere('description', 'like', '%' . request('search') . '%')
             ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
+    }
+
+    // Relationship To User
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
