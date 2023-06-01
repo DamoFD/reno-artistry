aae820dcb58914448aa2a22bd09631e92dd1df5d
DamoFD <voshall@yahoo.com>
2023-05-31 19:08:33 -0700

    register mobile friendly

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,43 +1,43 @@
 <x-layout>
 
     <section id="create-post">
-        <a href="/reno-artistry/public">Back</a>
-    <div>
-    <h1>Register</h1>
-    <p>Create an account to post your local Reno artwork.</p>
+        <a class="back color-white-1 color-primary-bg" href="/reno-artistry/public">Back</a>
+    <div class="container">
+    <h1 class="font-mont-bold color-primary">Register</h1>
+    <p class="font-roboto-regular color-secondary">Create an account to post your local Reno artwork.</p>
     <form method="POST" action="/reno-artistry/public/users">
         @csrf
         <label>Name</label>
-        <input value="{{ old('name') }}" type="text" name="name" placeholder="Example: John Doe" />
+        <input value="{{ old('name') }}" type="text" name="name" placeholder="John Doe" />
     
         @error('name')
-            <p>{{ $message }}</p>
+            <p class="error">{{ $message }}</p>
         @enderror
     
         <label>Email</label>
-        <input value="{{ old('email') }}" type="email" name="email" placeholder="Example: example@gmail.com" />
+        <input value="{{ old('email') }}" type="email" name="email" placeholder="example@gmail.com" />
         
         @error('email')
-            <p>{{ $message }}</p>
+            <p class="error">{{ $message }}</p>
         @enderror
         
         <label>Password</label>
         <input value="{{ old('password') }}" type="password" name="password" placeholder="Enter Password" />
         
         @error('password')
-            <p>{{ $message }}</p>
+            <p class="error">{{ $message }}</p>
         @enderror
         
         <label>Confirm Password</label>
         <input value="{{ old('password_confirmation') }}" type="password" name="password_confirmation" placeholder="Confirm Password" />
         
         @error('password_confirmation')
-            <p>{{ $message }}</p>
+            <p class="error">{{ $message }}</p>
         @enderror
         
-        <button type="submit">Sign Up</button>
+        <button class="color-white-1 font-rale-bold" type="submit">Sign Up</button>
     </form>
-    <p>Already have an account? <a href="/reno-artistry/public/login">Login</a></p>
+    <p class="account-text font-size-sm color-secondary font-roboto-regular">Already have an account? <a class="color-primary" href="/reno-artistry/public/login">Login</a></p>
     </div>
     </section>
     </x-layout>
