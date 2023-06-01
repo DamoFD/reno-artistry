499d73741bb437c8c80601e030292621e15e8aca
DamoFD <voshall@yahoo.com>
2023-05-31 21:55:59 -0700

    manage listings mobile responsive

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,4 +1,4 @@
 <form action="/reno-artistry/public/#listings" class="search-bar">
-    <input name="search" type="text" placeholder="Search Art in Reno..." />
+    <input name="search" type="text" placeholder="Search Reno Art..." />
     <button type="submit" class="search-button">Search</button>
 </form>
--- resources/views/posts/manage.blade.php
+++ resources/views/posts/manage.blade.php
@@ -1,27 +1,32 @@
 <x-layout>
 
     <section id="manage">
-        <a href="/reno-artistry/public">Back</a>
-        <h1>Manage Posts</h1>
-        <a href="/reno-artistry/public/posts/create">Add Post</a>
-        <div>
-            @unless($posts->isEmpty())
-            @foreach($posts as $post)
-            <div>
-            <h2>{{ $post->title }}</h2>
-            <p>{{ $post->created_at->format('F d, Y') }}</p>
-            <a href="/reno-artistry/public/posts/{{ $post->id }}/edit">Edit</a>
-            <form method="POST" action="/reno-artistry/public/posts/{{ $post->id }}">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>    
-            </div>
-            @endforeach
-            @else
-            <a href="/reno-artistry/public/posts/create">Add your first listing!</a>
-            @endunless
+        <a class="back color-white-1 color-primary-bg" href="/reno-artistry/public">Back</a>
+        <div class="container">
+            <h1 class="font-mont-bold color-primary">Manage Posts</h1>
+            <a class="btn color-white-1 color-primary-bg" href="/reno-artistry/public/posts/create">Add Post</a>
         </div>
+        @unless ($posts->isEmpty())
+            @foreach ($posts as $post)
+                <div class="item-container">
+                    <div class="item-text">
+                        <h2 class="font-rale-bold color-secondary">{{ $post->title }}</h2>
+                        <p class="font-roboto-regular color-secondary">{{ $post->created_at->format('F d, Y') }}</p>
+                    </div>
+                    <div class="btns">
+                    <a class="btn color-white-1 color-primary-bg"
+                        href="/reno-artistry/public/posts/{{ $post->id }}/edit">Edit</a>
+                    <form method="POST" action="/reno-artistry/public/posts/{{ $post->id }}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn color-white-1" type="submit">Delete</button>
+                    </form>
+                    </div>
+                </div>
+            @endforeach
+        @else
+            <a href="/reno-artistry/public/posts/create">Add your first listing!</a>
+        @endunless
     </section>
 
 </x-layout>
