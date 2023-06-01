691a2c30e5e0648cf8759ae561f0a524bcc4b36c
DamoFD <voshall@yahoo.com>
2023-06-01 13:15:51 -0700

    app mobile responsive

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,16 +4,16 @@
     <a class="back color-white-1 color-primary-bg" href="/reno-artistry/public">Back</a>
     <div class="container">
     <img src="{{ $post->image ? asset('storage/' . $post->image) : asset('/images/reno-artistry-logo.webp') }}" />
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->created_at->format('F d, Y') }}</p>
-    <h2>{{ $post->user->name }}</h2>
+    <h1 class="font-mont-bold color-primary">{{ $post->title }}</h1>
+    <p class="color-secondary font-roboto-regular">{{ $post->created_at->format('F d, Y') }}</p>
+    <h2 class="color-secondary font-rale-bold">{{ $post->user->name }}</h2>
     <x-post-tags :tagsCsv="$post->tags" />
-    <p>{{ $post->description }}</p>
+    <p class="font-roboto-regular color-secondary">{{ $post->description }}</p>
     @if(isset($post->email))
-    <a href="mailto:{{ $post->email }}">Contact</a>
+    <a class="btn color-primary-bg font-roboto-regular color-white-1" href="mailto:{{ $post->email }}">Contact</a>
     @endif
     @if(isset($post->website))
-    <a href="{{ $post->website }}">View Website</a>
+    <a class="btn color-primary-bg font-roboto-regular color-white-1" href="{{ $post->website }}">View Website</a>
     @endif
     </div>
 </section>
