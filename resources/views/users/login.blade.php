1a957727fb7c6ecc242b9edef8370e5ded54668b
DamoFD <voshall@yahoo.com>
2023-05-31 19:14:32 -0700

    login mobile friendly

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,30 +1,30 @@
 <x-layout>
 
     <section id="create-post">
-        <a href="/reno-artistry/public">Back</a>
-    <div>
-    <h1>Login</h1>
-    <p>Login to your account to post your local Reno artwork.</p>
+        <a class="back color-white-1 color-primary-bg" href="/reno-artistry/public">Back</a>
+    <div class="container">
+    <h1 class="font-mont-bold color-primary">Login</h1>
+    <p class="font-roboto-regular color-secondary">Login to your account to post your local Reno artwork.</p>
     <form method="POST" action="/reno-artistry/public/users/authenticate">
         @csrf
     
         <label>Email</label>
-        <input value="{{ old('email') }}" type="email" name="email" placeholder="Example: example@gmail.com" />
+        <input value="{{ old('email') }}" type="email" name="email" placeholder="example@gmail.com" />
         
         @error('email')
-            <p>{{ $message }}</p>
+            <p class="error">{{ $message }}</p>
         @enderror
         
         <label>Password</label>
         <input value="{{ old('password') }}" type="password" name="password" placeholder="Enter Password" />
         
         @error('password')
-            <p>{{ $message }}</p>
+            <p class="error">{{ $message }}</p>
         @enderror
         
-        <button type="submit">Sign In</button>
+        <button class="color-white-1 font-rale-bold" type="submit">Sign In</button>
     </form>
-    <p>Don't have an account? <a href="/reno-artistry/public/register">Register</a></p>
+    <p class="account-text font-size-sm color-secondary font-roboto-regular">Don't have an account? <a class="color-primary" href="/reno-artistry/public/register">Register</a></p>
     </div>
     </section>
     </x-layout>
